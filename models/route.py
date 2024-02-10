805b0bf01ef10d9e434b97fc9875c6fe8e62db89
eahristovaa5 <eahristovaa@gmail.com>
2024-02-10 13:20:10 +0200

    started_making_changes_but_still_being_in_the_middle_of_nowhere

--- models/route.py
+++ models/route.py
@@ -1,71 +1,92 @@
 from datetime import datetime, timedelta
-from models.schedule import Schedule
+from models.locations import Locations
 
 class Routes:
-    def __init__(self, id: int, date: datetime) -> None:
-        self._id = id  # check how to not mess up - random library 
-        self._date = date 
-        self._locations = []
-    
+    def __init__(self, id: int, start_location: str, end_location: str, start_date_time: datetime) -> None:
+        self._date_generator(start_date_time)
+
+        self._id = id  # check how to not mess up 
+        self._start_location = start_location
+        self._end_location = end_location
+        self._other_locations = []
+        self._start_date_time = start_date_time
+
+        # self._package = []  # to follow how to connect with model packages.py
+        # self._truck = []  # to follow how to connect with model truck.py
+
     @property
     def id(self):
         return self._id
     
     @property
-    def locations(self):
-        return tuple(self._locations)
+    def start_location(self):
+        return self._start_location
+    
+    @start_location.setter
+    def start_location(self, value):
+        if value not in Locations.locations:
+            raise ValueError('Please enter a supported location!')
+        
+        self._start_location = value
     
     @property
-    def date(self):
-        return self._date
+    def end_location(self):
+        return self._end_location
     
-    # shows the start location of the scheduled route and the departure time
-    def departure(self, departure_date: datetime):
-        return f'{departure_date}' # modify date accornig to prepared schedule
-    
-    # shows the other and the end locations of the scheduled route and the expected arrival time
-    def arrival(self, arrival_date: datetime):
-        return f'{arrival_date}' # modify date accornig to prepared schedule
-    
-    # add a location to a route which will be a scheduled one
-    def add_locaton(self, location: Schedule): 
-        if location not in Schedule.locations:
-            raise ValueError(f'Please enter a supported location.')
+    @end_location.setter
+    def end_location(self, value):
+        if value not in Locations.locations:
+            raise ValueError('Please enter a supported location!')
         
-        self._locations.append(location)
-
-    # removes a location to a route which will be a scheduled one if the max range has been exceeded
-    def remove_location(self, location: Schedule): 
-        pass
-
-    # shows a list of the scheduled routes
-    def info_route(self):
-        pass
-
-    # in the application_data.py - appends a route to the schedule if the route hasn't been created already (attribute: self._routes = [])
-    def create_route():
-        pass
-
-    # in the application_data.py - if the route we are looking for doeasn't exist it will show an error and that means we have to create one (attribute: self._routes = [])
-    def show_route(): 
-        pass
-
-    # in the application_data.py - checks if the route we are looking for exists (attribute: self._routes = [])
-    def is_route_scheduled(): 
-        pass 
+        self._end_location = value
     
-    # in the application_data.py - appends a package to an existing route (attribute: self._package_routes = [])
-    def add_package_to_route():
-        pass
+    @property
+    def other_locations(self):
+        return tuple(self._other_locations)
+    
+    @property
+    def start_date_time(self):
+        return self._start_date_time.
 
-    # in the application_data.py - removes a package from the existing route when end location has been reached (attribute: self._package_routes = [])
-    def remove_package_to_route():
-        pass
+    def date_generator(self, start_date_time):
+        start_date_time = datetime.today()
+        current = start_date_time .strftime('%d/%m %A @ %H:%M')
+        modified_start_hour = start_date_time .replace(hour=6)
+        current = modified_start_hour.strftime('%d/%m %A @ %H:%M')
+        modified_start_minute = modified_start_hour.replace(minute=00)
+        # current = modified_start_minute.strftime('%d/%m %A @ %H:%M')
+        current = modified_start_minute
+        return current.strftime('%d/%m %A @ %H:%M') 
+    
 
-    # in the application_data.py - appends a truck to an existing route (attribute: self._truck_routes = [])
-    def add_truck_to_route():
-        pass
+    # # shows a list of the scheduled routes
+    # def info_route(self):
+    #     pass
 
-    # in the application_data.py - removes a truck from the existing route when end location has been reached (attribute: self._truck_routes = [])
-    def remove_truck_to_route():
-        pass
+    # # in the application_data.py - appends a route to the schedule if the route hasn't been created already (attribute: self._routes = [])
+    # def create_route():
+    #     pass
+
+    # # in the application_data.py - if the route we are looking for doeasn't exist it will show an error and that means we have to create one (attribute: self._routes = [])
+    # def show_route(): 
+    #     pass
+
+    # # in the application_data.py - checks if the route we are looking for exists (attribute: self._routes = [])
+    # def is_route_scheduled(): 
+    #     pass 
+    
+    # # in the application_data.py - appends a package to an existing route (attribute: self._package_routes = [])
+    # def add_package_to_route():
+    #     pass
+
+    # # in the application_data.py - removes a package from the existing route when end location has been reached (attribute: self._package_routes = [])
+    # def remove_package_to_route():
+    #     pass
+
+    # # in the application_data.py - appends a truck to an existing route (attribute: self._truck_routes = [])
+    # def add_truck_to_route():
+    #     pass
+
+    # # in the application_data.py - removes a truck from the existing route when end location has been reached (attribute: self._truck_routes = [])
+    # def remove_truck_to_route():
+    #     pass
