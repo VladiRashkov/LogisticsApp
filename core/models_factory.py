a1b996dcb2a5b680eeb32a5c5be960ae6daf7741
eahristovaa5 <eahristovaa@gmail.com>
2024-02-10 13:18:50 +0200

    commiting_just_in_case

--- core/models_factory.py
+++ core/models_factory.py
@@ -2,7 +2,7 @@ from models.route import Routes
 
 class ModelsFactory:
         def __init__(self) -> None:
-                self._route_id = 1
+               self._route_id = 1
 
         def create_route(self, start_location: str, end_location: str):
                 route_id = self._route_id
