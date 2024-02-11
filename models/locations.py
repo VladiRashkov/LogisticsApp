850cf9b9f425d4dab4bd64ea57aa0b6112db5fb7
eahristovaa5 <eahristovaa@gmail.com>
2024-02-11 15:04:45 +0200

    made_changes_in_constants

--- models/locations.py
+++ models/locations.py
@@ -3,7 +3,7 @@ class Locations:
         'Sydney-Melbourne': 877, 'Sydney-Adelaide': 1376, 'Sydney-Alice Springs': 2762, 'Sydney-Bristbane': 909, 'Sydney-Darwin': 3935, 'Sydney-Perth': 4016,
             'Melbourne-Sydney': 877, 'Melbourne-Adelaide': 725, 'Melbourne-Alice Springs': 2255, 'Melbourne-Bristbane': 1765, 'Melbourne-Darwin': 3752, 'Melbourne-Perth': 3509,
             'Adelaide-Sydney': 1376, 'Adelaide-Melbourne': 725, 'Adelaide-Alice Springs': 1530, 'Adelaide-Bristbane': 1927, 'Adelaide-Darwin': 3027, 'Adelaide-Perth': 2785,
-            'Alice Springs-Sydney': 2762, 'Alice Springs-Melbourne': 2255, 'Alice Springs-Adelaide': 1530, 'Alice Springs-BRI': 2993, 'Alice Springs-Darwin': 1497, 'Alice Springs-Perth': 2481,
+            'Alice Springs-Sydney': 2762, 'Alice Springs-Melbourne': 2255, 'Alice Springs-Adelaide': 1530, 'Alice Springs-Bristbane': 2993, 'Alice Springs-Darwin': 1497, 'Alice Springs-Perth': 2481,
             'Bristbane-Sydney': 909, 'Bristbane-Melbourne': 1765, 'Bristbane-Adelaide': 1927, 'Bristbane-Alice Springs': 2993, 'Bristbane-Darwin': 3426, 'Bristbane-Perth': 4311,
             'Darwin-Sydney': 3935, 'Darwin-Melbourne': 3752, 'Darwin-Adelaide': 3027, 'Darwin-Alice Springs': 1497, 'Darwin-Bristbane': 1765, 'Darwin-Perth': 4025,
             'Perth-Sydney': 4016, 'Perth-Melbourne': 3509, 'Perth-Adelaide': 2785, 'Perth-Alice Springs': 2481, 'Perth-Bristbane': 4311, 'Perth-Darwin': 4025
@@ -13,7 +13,7 @@ class Locations:
         'Sydney-Melbourne': 10, 'Sydney-Adelaide': 15, 'Sydney-Alice Springs': 31, 'Sydney-Bristbane': 10, 'Sydney-Darwin': 45, 'Sydney-Perth': 46, 
         'Melbourne-Sydney': 10, 'Melbourne-Adelaide': 8, 'Melbourne-Alice Springs': 25, 'Melbourne-Bristbane': 20, 'Melbourne-Darwin': 43, 'Melbourne-Perth': 40, 
         'Adelaide-Sydney': 15, 'Adelaide-Melbourne': 8, 'Adelaide-Alice Springs': 17, 'Adelaide-Bristbane': 22, 'Adelaide-Darwin': 34, 'Adelaide-Perth': 32, 
-        'Alice Springs-Sydney': 31, 'Alice Springs-Melbourne': 25, 'Alice Springs-Adelaide': 17, 'Alice Springs-BRI': 34, 'Alice Springs-Darwin': 17, 'Alice Springs-Perth': 28, 
+        'Alice Springs-Sydney': 31, 'Alice Springs-Melbourne': 25, 'Alice Springs-Adelaide': 17, 'Alice Springs-Bristbane': 34, 'Alice Springs-Darwin': 17, 'Alice Springs-Perth': 28, 
         'Bristbane-Sydney': 10, 'Bristbane-Melbourne': 20, 'Bristbane-Adelaide': 22, 'Bristbane-Alice Springs': 34, 'Bristbane-Darwin': 39, 'Bristbane-Perth': 49, 
         'Darwin-Sydney': 45, 'Darwin-Melbourne': 43, 'Darwin-Adelaide': 34, 'Darwin-Alice Springs': 17, 'Darwin-Bristbane': 20, 'Darwin-Perth': 46, 
         'Perth-Sydney': 46, 'Perth-Melbourne': 40, 'Perth-Adelaide': 32, 'Perth-Alice Springs': 28, 'Perth-Bristbane': 49, 'Perth-Darwin': 46
