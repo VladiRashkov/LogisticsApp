ef6a1c239c3e166ee690a578bfb589b6184dc9d4
eahristovaa5 <eahristovaa@gmail.com>
2024-02-15 19:20:36 +0200

    okay

--- models/route.py
+++ models/route.py
@@ -1,6 +1,6 @@
 # import random
 from datetime import datetime, timedelta
-from constants.locations import Locations
+from locations import Locations
 
 class Route:
     id = 0
@@ -63,3 +63,26 @@ class Route:
                 f'Total distance: {self.distance()} km\n'
                 f'Start date: {self.start_date_time.strftime('%d/%m %A @ %H:%M')}\n'
                 f'Final ETA: {self.final_eta()}')
+    
+# example 2: creating a random schedule 
+import random
+
+# locations = ['Adelaide', 'Melbourne', 'Sydney', 'Bristbane', 'Alice Springs', 'Darwin', 'Perth']
+
+cities = Locations.locations
+    
+start_index = random.randint(0, 4)  
+end_index = random.randint(5, 6) + 1    
+cities = cities[start_index:end_index]
+
+num_cities = random.randint(2, len(cities))  
+num_routes = random.randint(1, 7)  
+    
+scheduled_routes = []
+for _ in range(num_routes):
+    selected_cities = random.sample(population=cities, k=num_cities)
+    route = Route(*selected_cities)
+    scheduled_routes.append(route)
+
+for route in scheduled_routes:
+    print(route.route_info())
