fd9fc920acede1cf30fc3aff4cb1352302dfe15c
eahristovaa5 <eahristovaa@gmail.com>
2024-02-17 14:40:32 +0200

    create_route_command

--- commands/create_route.py
+++ commands/create_route.py
@@ -1,12 +1,21 @@
 from core.application_data import ApplicationData
-from commands.validation_helpers import validate_params_count
+from core.models_factory import ModelsFactory
+from commands.base_command import BaseCommand
 
-class CreateRouteCommand:
-
-    def __init__(self, params, app_data: ApplicationData):
-        validate_params_count(params, 1)
-        self._params = params
-        self._app_data = app_data
+class CreateRouteCommand(BaseCommand):
+    def __init__(self, params: list[str], app_data: ApplicationData, models_factory: ModelsFactory):
+        super().__init__(params, app_data)
+        self._models_factory = models_factory
 
     def execute(self):
-        pass
+        start_location = self.params[0]
+        other_locations = self.params[1:]
+
+        route = self._models_factory.create_route(start_location, *other_locations)
+        self.app_data.add_route(route)
+
+        return f'Route with ID: {route.id} has been created!'
+
+
+# Input: CreateRoute AliceSprings Adelaide Sydney Melbourne Bristbane (n params)
+# Output: Route with ID: 1 has been created!
