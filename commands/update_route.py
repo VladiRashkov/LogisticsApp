c2c8535bee472a9bd6c444e7291f55a5b5776a2d
eahristovaa5 <eahristovaa@gmail.com>
2024-02-17 14:52:39 +0200

    updateroute

---
+++ commands/update_route.py
@@ -0,0 +1,12 @@
+from core.application_data import ApplicationData
+from commands.base_command import BaseCommand
+
+class UpdateRouteCommand(BaseCommand):
+    def __init__(self, params: list[str], app_data: ApplicationData):
+        super().__init__(params, app_data)
+
+    def execute(self):
+        pass
+
+# Input: UpdateRoute RouteId (1 param)
+# Output: Route with ID: 25 has been updated with status: scheduled!
