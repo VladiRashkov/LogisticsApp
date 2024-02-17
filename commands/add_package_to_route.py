96392fd5ea25c83406f0c7f2ecf60cd78350997a
eahristovaa5 <eahristovaa@gmail.com>
2024-02-17 14:35:48 +0200

    addpackagetoroute

--- commands/add_package_to_route.py
+++ commands/add_package_to_route.py
@@ -1,1 +1,12 @@
-# command - adding a package to route to follow
+from core.application_data import ApplicationData
+from commands.base_command import BaseCommand
+
+class AddPackageToRouteCommand(BaseCommand):
+    def __init__(self, params: list[str], app_data: ApplicationData):
+        super().__init__(params, app_data)
+    
+    def execute(self):
+        pass
+
+# Input: AddPackageToRoute RouteId PackageId weight (3 params)
+# Output: Package with ID: 21 has been added to route with ID: 25!
