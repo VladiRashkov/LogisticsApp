ee84ae7418e964dcfd88214d8d53b5bebf4aa805
eahristovaa5 <eahristovaa@gmail.com>
2024-02-18 21:12:57 +0200

    removetruckfromroute

--- commands/remove_truck_from_route.py
+++ commands/remove_truck_from_route.py
@@ -4,7 +4,7 @@ from commands.base_command import BaseCommand
 class RemoveTruckFromRouteCommand(BaseCommand):
     def __init__(self, params: list[str], app_data: ApplicationData):
         super().__init__(params, app_data)
-
+        
     def execute(self):
         route_id = int(self.params[0])
         truck_id = int(self.params[1])
