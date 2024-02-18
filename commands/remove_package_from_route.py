7d5a6d535e0b197d856453e4875fc0646474866e
eahristovaa5 <eahristovaa@gmail.com>
2024-02-18 12:00:42 +0200

    route

--- commands/remove_package_from_route.py
+++ commands/remove_package_from_route.py
@@ -6,7 +6,14 @@ class RemovePackageFromRouteCommand(BaseCommand):
         super().__init__(params, app_data)
 
     def execute(self):
-        pass
+        route_id = int(self.params[0])
+        package_id = int(self.params[1])
+
+        if self.app_data.remove_package(package_id):
+            return f'Package with ID: {package_id} has been removed from route with ID: {route_id}!'
+        else:
+            return f'Package with ID: {package_id} not found!'
+
 
 # Input: RemovePackageFromRoute RouteId PackageId (2 params)
 # Output: Package with ID: 21 has been removed from route with ID: 25!
