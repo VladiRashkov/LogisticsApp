2821230fd6de6b8d5d24df294c1774308fbbbab4
eahristovaa5 <eahristovaa@gmail.com>
2024-02-18 21:08:56 +0200

    commandfactory

--- core/command_factory.py
+++ core/command_factory.py
@@ -2,15 +2,16 @@ from commands.create_package import CreatePackageCommand
 from commands.register_customer import RegisterCustomer
 from commands.find_trucks import FindTrucks
 from commands.create_trucks import CreateTrucks
-# for route:
+# Route:
 from commands.add_package_to_route import AddPackageToRouteCommand
 from commands.add_truck_to_route import AddTruckToRouteCommand
 from commands.create_route import CreateRouteCommand
+from commands.find_route import FindRouteCommand
+from commands.info_route import InfoRouteCommand
 from commands.remove_package_from_route import RemovePackageFromRouteCommand
 from commands.remove_truck_from_route import RemoveTruckFromRouteCommand
-from commands.find_route import SearchRouteCommand
 from commands.update_route import UpdateRouteCommand
-from commands.Info_route import ViewRouteCommand
+from commands.view_route import ViewRouteCommand
 
 from core.application_data import ApplicationData
 
@@ -29,19 +30,21 @@ class CommandFactory:
             return CreateTrucks(params, self._app_data)
         if cmd.lower() == "findtrucks":
             return FindTrucks(params, self._app_data)
-        # for route:
+        # Route:
         if cmd.lower() == 'addpackagetoroute':
             return AddPackageToRouteCommand(params, self._app_data)
         if cmd.lower() == 'addtrucktoroute':
             return AddTruckToRouteCommand(params, self._app_data)
         if cmd.lower() == 'createroute':
             return CreateRouteCommand(params, self._app_data)
+        if cmd.lower() == 'findroute':
+            return FindRouteCommand(params, self._app_data)
+        if cmd.lower() == 'inforoute':
+            return InfoRouteCommand(params, self._app_data)
         if cmd.lower() == 'removepackagefromroute':
             return RemovePackageFromRouteCommand(params, self._app_data)
         if cmd.lower() == 'removetruckfromroute':
             return RemoveTruckFromRouteCommand(params, self._app_data)
-        if cmd.lower() == 'searchroute':
-            return SearchRouteCommand(params, self._app_data)
         if cmd.lower() == 'updateroute':
             return UpdateRouteCommand(params, self._app_data)
         if cmd.lower() == 'viewroute':
