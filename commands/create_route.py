4700bc45a2625288de403f7ef664ad53b8e5601f
eahristovaa5 <eahristovaa@gmail.com>
2024-02-18 21:09:46 +0200

    createroute

--- commands/create_route.py
+++ commands/create_route.py
@@ -1,23 +1,22 @@
 from core.application_data import ApplicationData
-from core.models_factory import ModelsFactory
 from commands.base_command import BaseCommand
+from models.route import Route
 
 class CreateRouteCommand(BaseCommand):
-    def __init__(self, params: list[str], app_data: ApplicationData, models_factory: ModelsFactory):
+    def __init__(self, params: list[str], app_data: ApplicationData):
         super().__init__(params, app_data)
-        self._models_factory = models_factory
-
+      
     def execute(self):
+        route_id = Route.id
         start_location = self.params[0]
         other_locations = self.params[1:]
 
-        route = self._models_factory.create_route(start_location, *other_locations)
+        route = Route(start_location, *other_locations)
         self.app_data.add_route(route)
 
-        return f'Route with ID: {route.id} has been created!'
+        return f'Route with ID: {route_id} has been created!'
 
 
 # Input: CreateRoute AliceSprings Adelaide Sydney Melbourne Bristbane (n params)
 # Output: Route with ID: 1 has been created!
-    
-# changes to follow?
+
