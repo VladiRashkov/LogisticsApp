75276e2b860d4e5204c26b5fe5123ea855ed4aed
eahristovaa5 <eahristovaa@gmail.com>
2024-02-18 21:01:17 +0200

    status

--- models/status.py
+++ models/status.py
@@ -14,3 +14,13 @@ class RouteStatus:
             return cls.order_route[idx + 1]
         else:
             return current
+        
+class PackageStatus:
+    ASSIGNED  = "Assigned"
+    UNASSIGNED = "Unassigned"
+    INPROGRESS = "In progress"
+    DELIVERED = "Delivered"
+
+class TruckStatus:
+    FREE = 'Free'
+    BUSY = 'Busy'
