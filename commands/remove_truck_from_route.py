adbde2fca457c8cda53c6997fdf253b1b26bcf15
eahristovaa5 <eahristovaa@gmail.com>
2024-02-19 22:16:30 +0200

    removetruck

--- commands/remove_truck_from_route.py
+++ commands/remove_truck_from_route.py
@@ -9,10 +9,14 @@ class RemoveTruckFromRouteCommand:
         route_id = int(self._params[0])
         truck_id = int(self._params[1])
 
-        if self._app_data.remove_truck(truck_id):
-            return f'Truck with ID: {truck_id} has been removed from route with ID: {route_id}!'
-        else:
+        route = self._app_data.find_route_id(route_id)
+        if route is None:
+            return f'Route with ID: {route_id} not found!'
+        
+        truck = self._app_data.find_truck_id(truck_id)
+        if truck is None:
             return f'Truck with ID: {truck_id} not found!'
+        
+        route.remove_truck(truck)
 
-# Input: RemoveTruckFromRoute RouteId TruckId  (2 params)
-# Output: Truck with ID: 1007 has been removed from route with ID: 25!
+        return f'Truck with ID: {truck_id} has been removed from route with ID: {route_id}!'
