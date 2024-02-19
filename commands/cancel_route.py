0282b0c4c1b99e7ad0468597f054628a23a6714a
eahristovaa5 <eahristovaa@gmail.com>
2024-02-19 15:14:27 +0200

    canceltoute

---
+++ commands/cancel_route.py
@@ -0,0 +1,16 @@
+from core.application_data import ApplicationData
+from models.route import Route
+
+
+class CancelRouteCommand:
+    def __init__(self, params: list[str], app_data: ApplicationData):
+        self._params = params
+        self._app_data = app_data
+      
+    def execute(self):
+        route_id = int(self._params[0])
+        
+        if self._app_data.cancel_route(route_id):
+            return f'Route with ID: {route_id} has been cancelled'
+        else:
+            return f'Route with ID: {route_id} not found!'
