10ce46bd03365282f1305e06c020333841131d83
eahristovaa5 <eahristovaa@gmail.com>
2024-02-19 10:33:51 +0200

    findroute

--- commands/find_route.py
+++ commands/find_route.py
@@ -6,9 +6,16 @@ class FindRouteCommand(BaseCommand):
         super().__init__(params, app_data)
         
     def execute(self):
-        pass
+        start_location = self.params[0]
+        end_location = self.params[1]
 
-# Input:  SearchRoute AliceSprings Adelaide (2 params)
+        route = self.app_data.find_route_by_locations(start_location, end_location)
+
+        if route is None:
+            return f'Route from {start_location} to {end_location} not found!'
+        else:
+            return f'Route locations: {route.locations()}'
+
+# Input:  FindRoute AliceSprings Adelaide (2 params)
 # Output: 
 # Route locations: Alice Spring (10/02 Saturday @ 06:00) → Adelaide (10/02 Saturday @ 20:00) → Sydney (11/02 Sunday @ 18:00) -> Melbourne (12/02 Monday @ 14:00) -> Brisbane (13/02 Tuesday @ 14:00)
-
