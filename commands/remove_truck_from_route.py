9a7b6cc7e1f5ff64432cdc39ac6b0fee484ed762
eahristovaa5 <eahristovaa@gmail.com>
2024-02-19 11:11:32 +0200

    removetruck

--- commands/remove_truck_from_route.py
+++ commands/remove_truck_from_route.py
@@ -1,15 +1,15 @@
 from core.application_data import ApplicationData
-from commands.base_command import BaseCommand
 
-class RemoveTruckFromRouteCommand(BaseCommand):
+class RemoveTruckFromRouteCommand:
     def __init__(self, params: list[str], app_data: ApplicationData):
-        super().__init__(params, app_data)
+        self._params = params
+        self._app_data = app_data
         
     def execute(self):
-        route_id = int(self.params[0])
-        truck_id = int(self.params[1])
+        route_id = int(self._params[0])
+        truck_id = int(self._params[1])
 
-        if self.app_data.remove_truck(truck_id):
+        if self._app_data.remove_truck(truck_id):
             return f'Truck with ID: {truck_id} has been removed from route with ID: {route_id}!'
         else:
             return f'Truck with ID: {truck_id} not found!'
