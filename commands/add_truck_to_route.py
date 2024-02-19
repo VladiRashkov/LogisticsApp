e60bb8f046cdc2d45aaec6b272b8dee338f1941e
eahristovaa5 <eahristovaa@gmail.com>
2024-02-19 22:13:01 +0200

    addtruck

--- commands/add_truck_to_route.py
+++ commands/add_truck_to_route.py
@@ -9,13 +9,13 @@ class AddTruckToRouteCommand:
         route_id = int(self._params[0])
         truck_id = int(self._params[1])
 
-        route = self._app_data.find_route(route_id)
+        route = self._app_data.find_route_id(route_id)
         if route is None:
             return f'Route with ID: {route.id} not found!'
         
-        route.add_truck(truck_id)
+        truck = self._app_data.find_truck_id(truck_id)
+        if truck is None:
+            return f'Truck with ID: {truck_id} not found!'
+
+        route.add_truck(truck)
         return f'Truck with ID: {truck_id} has been added to route with ID: {route_id}'
-
-
-# Input: AddTruckToRoute RouteId TruckId  (2 params)
-# Output: Truck with ID: 1007 has been added to route with ID: 25!
