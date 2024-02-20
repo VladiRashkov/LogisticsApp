a860fd3c9ba6867b033a64bbdcf5e020870233f0
AlexVidenov1 <alexander_videnov@yahoo.com>
2024-02-20 11:15:36 +0200

    info_truck change

--- commands/info_truck.py
+++ commands/info_truck.py
@@ -10,7 +10,7 @@ class InfoTruckCommand:
         truck_info = next((truck for truck in self.app_data._trucks if truck._truck_id == truck_id), None)
         if truck_info:
             info_message = f'\n Information for Truck {truck_info._truck_id}: \n Status: {truck_info._status} | Capacity: {truck_info._capacity} | Range: {truck_info._range}'
-            print(info_message)
+
             return info_message
         else:
-            print(f'Truck ID not found')
+            return 'Truck ID not found'
